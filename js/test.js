4964c97fb86a9defbf1a6dae03448ae28061e51b
babidisrc <thaissa.linguika@gmail.com>
2024-11-10 23:31:52 -0300

    test1

---
+++ js/test.js
@@ -0,0 +1,28 @@
+export const apps = {
+    "about-me": {
+        defaultSize: {
+            width: 696,
+            height: 454
+        },
+        minSize: {
+            width: 200,
+            height: 130
+        }
+    },
+    "archive": {
+        defaultSize: {
+            width: 636,
+            height: 694
+        },
+        minSize: {
+            width: 200,
+            height: 150
+        }
+    },
+    "media-player": {
+        defaultSize: {
+            width: 500,
+            height: 320
+        }
+    }
+}
